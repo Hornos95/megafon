652df70be7388fe7d310abd43142d0256bc00862
Stas <workunow_stas@mail.ru>
2019-10-21 17:30:43 +0400

    fixes #3

--- src/main/java/ru/itpark/service/CartService.java
+++ src/main/java/ru/itpark/service/CartService.java
@@ -6,20 +6,21 @@ import ru.itpark.model.Rate;
 import ru.itpark.model.TurnOnRate;
 
 public class CartService {
-private Rate[] rates = new Rate[10];
-private int size;
-public void add(Rate rate){
-rates[size++]= rate;
-if (rate instanceof TurnOnRate){
-TurnOnRate turnOn=(TurnOnRate) rate;
-}
-if (rate instanceof OtherDevice){
-OtherDevice device=(OtherDevice) rate;
-}
-if (rate instanceof OtherRate){
-OtherRate otherRate=(OtherRate) rate;
-}
-}
+    private Rate[] rates = new Rate[10];
+    private int size;
+
+    public void add(Rate rate) {
+        rates[size++] = rate;
+        if (rate instanceof TurnOnRate) {
+            TurnOnRate turnOn = (TurnOnRate) rate;
+        }
+        if (rate instanceof OtherDevice) {
+            OtherDevice device = (OtherDevice) rate;
+        }
+        if (rate instanceof OtherRate) {
+            OtherRate otherRate = (OtherRate) rate;
+        }
+    }
 
     public Rate[] getRates() {
         return rates;
